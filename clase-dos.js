5f33279d095570e89591170d263eb7fe88f55abb
marcreativo <marcreativo@gmail.com>
2019-04-11 22:05:11 -0300

    clase dos, objetos y arrays

---
+++ clase-dos.js
@@ -0,0 +1,29 @@
+var a = 'stark'
+var b = 'lannister'
+var c = 'mormont'
+var pos = 1
+
+// creación de un array con valores específico
+var list = ['uno', 'dos', 'tres']
+// creación de un array usando variable
+var houses = [a, b, c]
+
+// imprimimos un array
+console.log(houses)
+// imprimimos un array accediendo un índice específico
+console.log(houses[pos])
+// podemos acceder posiciones de un string, ya que internamente es un array
+console.log(a[0])
+
+// definición de un objeto
+var joffrey = {
+  age:15,
+  lastName:'lannister',
+  isAlive: false
+}
+
+// imprimimos en consola una propiedad específica del objeto
+console.log(joffrey.age)
+
+
+
